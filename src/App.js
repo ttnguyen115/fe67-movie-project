ff38c119ca90d520d18ac6db0252c8df65af2b8e
Trung Nguyen <ngtrung309@gmail.com>
2021-09-22 22:15:20 +0700

    notfound screen done with responsive

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Route, Switch } from "react-router";
 import Detail from './pages/Detail';
 import Home from './pages/Home';
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
     <Switch>
-      <Route path="/home" component={Home} />
-      <Route path="/" component={Detail} />
+      <Route path="/movie" component={Home} />
+      <Route path="/movie/:id" component={Detail} />
+
+      <Route path="*" component={NotFound} />
     </Switch>
   );
 }
