12ddbd2f51027285c3da873057d4e5d1908322a8
ttnguyen115 <thanhtrung1012000@gmail.com>
2021-10-26 23:48:30 +0700

    manual upload

--- src/pages/Admin/AddMovie/index.jsx
+++ src/pages/Admin/AddMovie/index.jsx
@@ -1,25 +1,44 @@
-import { InboxOutlined } from "@ant-design/icons";
+import { LoadingOutlined, PlusOutlined } from "@ant-design/icons";
 import {
-    Form,
-    Upload,
-    Row,
+    Button,
     Col,
-    Input,
     DatePicker,
+    Form,
+    Input,
     InputNumber,
+    message,
+    Row,
     Select,
     Switch,
-    Button,
+    Upload,
 } from "antd";
 import React from "react";
 import { addNewRules } from "../../../constants/formRules";
+import { getBase64 } from "../../../helpers/getBase64";
 import "./style.scss";
 
+// const beforeUpload = (file) => {
+//     const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
+//     if (!isJpgOrPng) {
+//         message.error("You can only upload JPG/PNG file!");
+//     }
+//     const isLt2M = file.size / 1024 / 1024 < 2;
+//     if (!isLt2M) {
+//         message.error("Image must smaller than 2MB!");
+//     }
+//     return isJpgOrPng && isLt2M;
+// };
+
 const AddMovie = () => {
+    const [loadingImg, setLoadingImg] = React.useState(false);
+    const [previewImage, setPreviewImage] = React.useState("");
+    const [fileList, setFileList] = React.useState([]);
+    const [isHot, setisHot] = React.useState(true);
     const [status, setStatus] = React.useState({
         SapChieu: false,
         DangChieu: false,
     });
+
     const normFile = (e) => {
         console.log("Upload event:", e);
 
@@ -30,14 +49,25 @@ const AddMovie = () => {
         return e && e.fileList;
     };
 
+    const getFormData = (object) => {
+        const formData = new FormData();
+        Object.keys(object).forEach(key => formData.append(`${key}`, `${object[key]}`));
+        return formData;
+    }
+
     const onFinish = (values) => {
+        console.log(values);
         const dataValues = {
             ...values,
             ngayKhoiChieu: values["ngayKhoiChieu"].format("DD/MM/YYYY"),
             SapChieu: status.SapChieu,
             DangChieu: status.DangChieu,
+            hot: isHot,
+            File: fileList[0],
         };
-        console.log("Success:", dataValues);
+        console.log(dataValues);
+        
+        console.log("Success:", getFormData(dataValues));
     };
 
     const onFinishFailed = (errorInfo) => {
@@ -60,6 +90,34 @@ const AddMovie = () => {
         }
     };
 
+    const handleHotSwitch = (value) => setisHot(value);
+
+    const handleUpload = () => {
+        const formData = new FormData();
+        formData.append("File", fileList[0]);
+    };
+
+    const uploadProps = {
+        onRemove: (file) => {
+            const index = fileList.indexOf(file);
+            const newFileList = fileList.slice();
+            newFileList.splice(index, 1);
+            setFileList(newFileList);
+        },
+        beforeUpload: (file) => {
+            setFileList([...fileList, file]);
+            return false;
+        },
+        fileList,
+    };
+
+    const uploadButton = (
+        <div>
+            {loadingImg ? <LoadingOutlined /> : <PlusOutlined />}
+            <div style={{ marginTop: 8 }}>Upload</div>
+        </div>
+    );
+
     return (
         <div className="add--movie__container">
             <div className="add--movie">
@@ -76,33 +134,31 @@ const AddMovie = () => {
                     <Row>
                         <Col span={24}>
                             <Row>
-                                <Col span={24} md={10} className="dragger--cover">
+                                <Col
+                                    span={24}
+                                    md={10}
+                                    className="dragger--cover"
+                                >
                                     <Form.Item
-                                        name="hinhAnh"
+                                        name="File"
                                         valuePropName="fileList"
                                         getValueFromEvent={normFile}
                                         className="form--dragger"
                                     >
                                         <Upload.Dragger
-                                            name="files"
-                                            action="/upload.do"
+                                            {...uploadProps}
+                                            name="File"
                                         >
-                                            <p className="ant-upload-drag-icon">
-                                                <InboxOutlined />
-                                            </p>
-                                            <p className="ant-upload-text">
-                                                Click or drag file to this area
-                                                to upload
-                                            </p>
-                                            <p className="ant-upload-hint">
-                                                Support for a single or bulk
-                                                upload.
-                                            </p>
+                                            {uploadButton}
                                         </Upload.Dragger>
                                     </Form.Item>
                                 </Col>
 
-                                <Col span={24} md={14} className="content--cover">
+                                <Col
+                                    span={24}
+                                    md={14}
+                                    className="content--cover"
+                                >
                                     <Row>
                                         <Col span={24}>
                                             <Form.Item
@@ -124,7 +180,11 @@ const AddMovie = () => {
                                                 <Input.TextArea placeholder="Description" />
                                             </Form.Item>
                                         </Col>
-                                        <Col span={24} lg={8} className="input--first">
+                                        <Col
+                                            span={24}
+                                            lg={8}
+                                            className="input--first"
+                                        >
                                             <Form.Item
                                                 className="form--group"
                                                 name="ngayKhoiChieu"
@@ -139,7 +199,11 @@ const AddMovie = () => {
                                                 />
                                             </Form.Item>
                                         </Col>
-                                        <Col span={24} lg={8} className="input--second">
+                                        <Col
+                                            span={24}
+                                            lg={8}
+                                            className="input--second"
+                                        >
                                             <Form.Item
                                                 className="form--group"
                                                 name="danhGia"
@@ -155,7 +219,11 @@ const AddMovie = () => {
                                                 />
                                             </Form.Item>
                                         </Col>
-                                        <Col span={24} lg={8} className="input--last">
+                                        <Col
+                                            span={24}
+                                            lg={8}
+                                            className="input--last"
+                                        >
                                             <Form.Item
                                                 label="Status"
                                                 className="form--group"
@@ -195,7 +263,10 @@ const AddMovie = () => {
                                                 <span className="mr-4 text-white">
                                                     Hot
                                                 </span>
-                                                <Switch checked />
+                                                <Switch
+                                                    defaultChecked
+                                                    onChange={handleHotSwitch}
+                                                />
                                             </Form.Item>
                                         </Col>
                                         <Col span={24}>
@@ -216,6 +287,7 @@ const AddMovie = () => {
                                         <Button
                                             type="primary"
                                             htmlType="submit"
+                                            onClick={handleUpload}
                                         >
                                             Submit
                                         </Button>
